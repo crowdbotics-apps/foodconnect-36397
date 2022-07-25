9891a34f401fb05cfb2ff293d02179bf00462e8a
Crowdbotics <team@crowdbotics.com>
2022-07-25 03:03:58 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "foodconnect_36397",
+  url: "https://foodconnect_36397.botics.co",
+  api: "https://foodconnect_36397.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
